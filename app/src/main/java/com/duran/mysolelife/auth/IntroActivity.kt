800010f66cb014098d6fca3b0f1ccda57cfedb21
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-10-04 17:38:56 +0900

    Intro -> Button  Move(Login Button -> LoginActivity / Join Button -> JoinActivity)

--- app/src/main/java/com/duran/mysolelife/auth/IntroActivity.kt
+++ app/src/main/java/com/duran/mysolelife/auth/IntroActivity.kt
@@ -1,12 +1,32 @@
 package com.duran.mysolelife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.duran.mysolelife.R
+import com.duran.mysolelife.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
+    private val btnLogin by lazy { binding.loginBtn }
+    private val btnJoin by lazy { binding.joinBtn }
+    private val btnNoAccount by lazy { binding.noAccountBtn }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnJoin.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
